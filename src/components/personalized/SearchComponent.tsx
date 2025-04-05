185d051cd10728edb29fd96d68c3514cebd21202
Eduardo XVlll <88681044+EduardoHead18@users.noreply.github.com>
2025-04-04 18:14:04 -0600

    refactor: Improve layout structure of SearchComponent for better responsiveness

--- src/components/personalized/SearchComponent.tsx
+++ src/components/personalized/SearchComponent.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { SearchIcon } from "lucide-react";
 import { useStoreSearch } from "@/hooks/useStore";
 
@@ -11,19 +10,27 @@ export const SearchComponent = () => {
 
   return (
     <div className="w-full mb-5">
-      <form onSubmit={handleSubmit} className="relative w-auto md:w-[400px]">
-        <SearchIcon
-          size={20}
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-        />
-        <input
-          type="text"
-          name="search"
-          placeholder="Buscar por ID, nombre, apellido, servicio..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="w-full pl-10 pr-4 py-2 border rounded-md "
-        />
+      <form onSubmit={handleSubmit} className="flex flex-row  w-auto gap-8 ">
+        <div className="relative md:w-[400px]">
+          <SearchIcon
+            size={20}
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+          />
+          <input
+            type="text"
+            name="search"
+            placeholder="Buscar por ID, nombre, apellido, servicio..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full pl-10 pr-4 py-2 border rounded-md "
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-black text-white hover:bg-gray-800 transition-colors duration-300 hover:cursor-pointer rounded-md px-4 py-2 flex items-center gap-2"
+        >
+          Buscar
+        </button>
       </form>
     </div>
   );
