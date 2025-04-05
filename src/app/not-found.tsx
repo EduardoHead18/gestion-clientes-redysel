a8c3a916eb7d86b65111d7a572d2fe83b38824b8
Eduardo XVlll <88681044+EduardoHead18@users.noreply.github.com>
2025-04-04 20:01:00 -0600

    add: Update NotFound component layout for improved user experience

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-9xl font-bold">404</h1>
+      <p className="mb-6 text-2xl">Not Found</p>
+      <Link className="hover:text-blue-500" href="/">
+        Regresar al Login
+      </Link>
     </div>
   );
 }
