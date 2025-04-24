b0f44750f7999fd19de90213deb476c83ab51801
Eduardo XVlll <88681044+EduardoHead18@users.noreply.github.com>
2025-04-24 15:54:00 -0600

    fix: Remove token state management and cookie retrieval from the layout component to prevent it from re-rendering every time a page is changed.

--- src/app/inicio/layout.tsx
+++ src/app/inicio/layout.tsx
@@ -2,34 +2,13 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { useEffect, useState } from "react";
-import { getServerCookie } from "@/services/services-api";
-import { useRouter } from "next/navigation";
 
 export default function LayoutHome({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [token, setToken] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const getCookie = async () => {
-      const response = await getServerCookie();
-      if (typeof response === "boolean") {
-        if (response === true) {
-          setToken(true);
-        } else {
-          router.push("/auth/login");
-        }
-      }
-    };
-
-    getCookie();
-  }, [router]);
-
-  return token ? (
+  return (
     <SidebarProvider>
       <AppSidebar variant="inset" />
       <SidebarInset>
@@ -43,5 +22,5 @@ export default function LayoutHome({
         </div>
       </SidebarInset>
     </SidebarProvider>
-  ) : null;
+  );
 }
